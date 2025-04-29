c95ba06d7e71bbbb72ede01ce9b3b7dbb749f8df
Adebisi Joseph <adebisijoe@gmail.com>
2025-04-29 14:30:28 +0100

    rerun

--- services/auth/auth_service.py
+++ services/auth/auth_service.py
@@ -11,7 +11,6 @@ ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
