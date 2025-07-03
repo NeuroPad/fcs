2ab9b02c8b003dd692b62052991579f984519aba
Adebisi Joseph <adebisijoe@gmail.com>
2025-07-03 14:19:39 +0100

    setup

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 sleep 5
 
 # Run database setup
-python /app/setup_db.py
+python setup_db.py
 
 # Run migrations
-python -m alembic upgrade head
+alembic upgrade head
 
 # Start the application
-python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --loop asyncio 
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --loop asyncio 
